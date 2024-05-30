43141037e1983b9b78f68bbbe55119b52e8b878b
tayuya74 <lyna-93@mail.ru>
2024-05-31 00:15:00 +0500

    add for and if

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-let money = prompt("Ваш бюджет на месяц?")
+let money = +prompt("Ваш бюджет на месяц?")
 let time = prompt("Введите дату в формате YYYY-MM-DD")
 let expensesName = prompt("Введите обязательную статью расходов в этом месяце")
 let expensesMoney = prompt("Во сколько обойдется?")
-let expensesName2 = prompt("Введите обязательную статью расходов в этом месяце")
-let expensesMoney2 = prompt("Во сколько обойдется?")
+
 let appData = {
   budget: parseFloat(money),
   timeData: time,
-  expenses: {
-    [expensesName]: parseFloat(expensesMoney),
-    [expensesName2]: parseFloat(expensesMoney2),
-  },
+  expenses: {},
   optionalExpenses: {},
   income: [],
   savings: false
 }
 
-alert(appData.budget / 30)
+for (let i = 1; i < 2; i++) {
+  let expensesName = prompt("Введите обязательную статью расходов в этом месяце")
+  let expensesMoney = prompt("Во сколько обойдется?")
+  
+  if (typeof(expensesName) != null && typeof(expensesMoney) != null
+  && expensesName != '' && expensesMoney != '') {
+    console.log('done')
+    appData.expenses[expensesName] = parseFloat(expensesMoney);
+  } else {}
+
+}
+
+appData.moneyPerDay = Math.round(appData.budget / 30)
+alert('Бюджет на один день: ' + appData.moneyPerDay)
+
+
