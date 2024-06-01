bb2962213429d500bfb8f861e4ead4be7cd13e7e
tayuya74 <lyna-93@mail.ru>
2024-06-02 02:05:21 +0500

    add optionalExpenses & checkSavings

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let money = +prompt("Ваш бюджет на месяц?")
-let time = prompt("Введите дату в формате YYYY-MM-DD")
-let expensesName = prompt("Введите обязательную статью расходов в этом месяце")
-let expensesMoney = prompt("Во сколько обойдется?")
+let money, time;
+
+function start () {
+  money = +prompt("Ваш бюджет на месяц?");
+  time = prompt("Введите дату в формате YYYY-MM-DD");  
+
+  while(isNaN(money) || money == '' || money == null) {
+    money = +prompt("Ваш бюджет на месяц?");
+  }
+}
+
+start()
 
 let appData = {
   budget: parseFloat(money),
@@ -9,22 +17,55 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true
 }
 
-for (let i = 1; i < 2; i++) {
-  let expensesName = prompt("Введите обязательную статью расходов в этом месяце")
-  let expensesMoney = prompt("Во сколько обойдется?")
-  
-  if (typeof(expensesName) != null && typeof(expensesMoney) != null
-  && expensesName != '' && expensesMoney != '') {
-    console.log('done')
-    appData.expenses[expensesName] = parseFloat(expensesMoney);
-  } else {}
-
+function expenses() {
+  for (let i = 0; i < 2; i++) {
+    let expensesName = prompt("Введите обязательную статью расходов в этом месяце")
+    let expensesMoney = prompt("Во сколько обойдется?")
+    
+    if (typeof(expensesName) != null && typeof(expensesMoney) != null
+    && expensesName != '' && expensesMoney != '') {
+      console.log('done')
+      appData.expenses[expensesName] = parseFloat(expensesMoney);
+    } else {
+      i--
+    }
+  }
 }
+expenses()
 
-appData.moneyPerDay = Math.round(appData.budget / 30)
-alert('Бюджет на один день: ' + appData.moneyPerDay)
+function dayBudget() {
+  appData.moneyPerDay = Math.round(appData.budget / 30)
+  alert('Бюджет на один день: ' + appData.moneyPerDay)
+}
+dayBudget()
 
+
+function checkSavings() {
+   if (appData.savings == true) {
+    let save = +prompt('Какова сумма накоплений?')
+    let persent = +prompt('Под какой процент?')
+    appData.monthIncome = Math.round(save/12/100*persent)
+    alert('Доход в месяц с депозита: ' +  appData.monthIncome)
+   }
+}
+checkSavings()
+
+function optionalExpenses() {
+  for (let i = 1; i < 4; i++) {
+    let optExpensesName = prompt("Статья необязательных расходов?")
+    
+    if (typeof(optExpensesName) != null && optExpensesName != '') {
+      console.log('done')
+      appData.optionalExpenses[i] = optExpensesName;
+    } else {
+      i--
+    }
+  }
+}
+optionalExpenses()
+
+console.log(appData)
 
